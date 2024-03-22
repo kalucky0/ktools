aaedfe1363f76bbaf15e00567c819aa8d362e54e
kalucky0 <kalucki23@gmail.com>
2024-03-22 13:23:16 +0100

    Add documentation

--- lib/ktools.dart
+++ lib/ktools.dart
@@ -1,9 +1,15 @@
+/// A library for generating Dart code from assets and translations.
 library ktools;
 
 import 'src/config.dart';
 import 'src/assets_gen.dart';
 import 'src/translations_gen.dart';
 
+/// Runs the specified action based on the given [action] parameter.
+///
+/// This method reads the configuration using [Config.read] and performs the
+/// action based on the value of [action]. If [action] is
+/// not recognized, it prints an error message.
 Future<void> run(String action) async {
   final config = await Config.read();
   switch (action) {
